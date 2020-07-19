a826b7ead86538fedccb5b7e2aed98ae930c0d2e
mesinger <uintptr96@gmail.com>
2020-07-19 14:43:07 +0200

    Initial commit

---
+++ Jenkins.Demo.Lib.Test/CalculatorTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Jenkins.Demo.Lib.Test
+{
+    public class CalculatorTests
+    {
+        private readonly Calculator sut = new Calculator();
+
+        [Test]
+        public void ItShallAdd()
+        {
+            var a = 10;
+            var b = 20;
+
+            var expected = a + b;
+            var actual = sut.Add(a, b);
+
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [Test]
+        public void ItShallSub()
+        {
+            var a = 10;
+            var b = 20;
+
+            var expected = a - b;
+            var actual = sut.Sub(a, b);
+
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [Test]
+        public void ItShallMul()
+        {
+            var a = 10;
+            var b = 20;
+
+            var expected = a * b;
+            var actual = sut.Mul(a, b);
+
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [Test]
+        public void ItShallDiv()
+        {
+            var a = 20;
+            var b = 10;
+
+            var expected = a / b;
+            var actual = sut.Div(a, b);
+
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [Test]
+        public void ItShallThrowOnDivByZero()
+        {
+            var a = 20;
+            var b = 0;
+
+            Assert.Throws<ArgumentException>(() => sut.Div(a, b));
+        }
+    }
+}
---
+++ Jenkins.Demo.Lib/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Jenkins.Demo.Lib
+{
+    public class Calculator
+    {
+        public int Add(int a, int b) => a + b;
+        public int Sub(int a, int b) => a - b;
+        public int Mul(int a, int b) => a * b;
+
+        public int Div(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new ArgumentException("Denominator may not be 0");
+            }
+            
+            return a / b;
+        }
+    }
+}
